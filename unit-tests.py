e96856e8c7a732256e008462b69f5bbe00744bb1
Oskar Person <oskar.person@hotmail.com>
2020-03-05 18:15:10 +0100

    ignore data/

--- unit-tests.py
+++ unit-tests.py
@@ -1,6 +1,7 @@
 import unittest
 from corpgame import Game
 game = Game()
+
 class TestGame(unittest.TestCase):
     def test_class_initiation(self):
         self.assertTrue(game.__class__ is type(game))
