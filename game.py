1cbe9f3d48b977047b7eca48247b964de2ab20fb
Oskar Person <oskar.person@hotmail.com>
2020-03-18 22:44:49 +0100

    CLI with iterations

--- game.py
+++ game.py
@@ -1,11 +1,14 @@
 import plac, corpgame
 from logger import log
 
-log.setLevel("info")
 
 
-@plac.annotations(number_of_players=("Amount of players", "option", "n", int))
-def main(number_of_players=3):
+
+@plac.annotations(number_of_players=("Amount of players", "option", "n", int),
+                    log_level=("Preferred level of logging", "option", "log", str),
+                    timesteps=("Amount of rounds to be played", "option", "t", int))
+def main(number_of_players=3, timesteps=2, log_level="warning"):
+    log.setLevel(log_level)
     manager = corpgame.GameManager(number_of_players)
     players = manager.get_random_players()
     # players = [[100, 100], [100, 100], [100, 100], [100, 100]]
@@ -14,13 +17,15 @@ def main(number_of_players=3):
         "topology": "fully_connected"
     }
     game = corpgame.PolymatrixGame(**game_settings)
-    log.info(f" Network edges: {game.network.edges}")
-    game.play(manager.get_random_strategy_profile())
-    log.info(f" Game state {game.state.tolist()}")
-    game.play(manager.get_random_strategy_profile())
-    log.info(f" Game state {game.state.tolist()}")
-    log.info(f"Simulated for {amount} players")
+    log.info(f"Simulated for {number_of_players} players")
+    log.info(f" Network topology: {game_settings['topology']}")
+    print(f" Game state {game.state.tolist()}")
+    for i in range(timesteps):
+        game.play(manager.get_random_strategy_profile())
+        print(f" Strategy profile: {game.strategy_profile}")
+        print(f" Game state {game.state.tolist()}")
+    
 
 if __name__ == "__main__":
-    plac.call(number_of_players)
+    plac.call(main)
     
