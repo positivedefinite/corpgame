572ed9e0eeca7328a3ca03be49f15a454d0e52ba
Oskar Person <oskar.person@hotmail.com>
2020-04-23 11:02:55 +0200

    Refactor

--- corpgame.py
+++ corpgame.py
@@ -4,87 +4,8 @@ import numpy as np
 from logger import log
 from player import Player
 from network import Network
+from multiplayergame import MultiplayerGame
 
-class MultiplayerGame:
-    """ A multiplayer game with state vector for each player """
-
-    def __init__(self, start_populations_matrix=[], topology="fully_connected", alpha=0.5, log_level="info"):
-        self.alpha = alpha
-        self.players = None
-        self.network = None
-        self.loss_velocity = None
-        self.strategy_profile = None
-        self.payoff_matrix = None
-        self.payoffs = {}
-        self.state = None
-        self.nash = {}
-        log.setLevel(log_level)
-        if start_populations_matrix != []:
-            self.initiate_players(start_populations_matrix=start_populations_matrix)
-            self.get_state()
-            self.players_indices = [player.index for player in self.players]
-            log.info(
-                f"{self.__class__}.__init__() players indexed {self.players_indices}"
-            )
-            self.network = Network(nodes=self.players_indices, topology=topology)
-        else:
-            log.warning(
-                f"{self.__class__}.__init__() players and network not initiated"
-            )
-
-    def initiate_players(self, start_populations_matrix: list, player_names_list=None):
-        """ Adds players to the game one by one """
-        assert (
-            self.players == None
-        )  # make sure that there aren't any players in the game
-        assert type(start_populations_matrix) == list
-        assert all(
-            len(x) == len(start_populations_matrix[0]) for x in start_populations_matrix
-        )
-        players_list = []
-        for i, population_vector in enumerate(start_populations_matrix):
-            players_list.append(Player(population_vector=population_vector, index=i))
-        self.players = players_list
-        return True
-
-    def get_state(self):
-        """ Extract state of each player and concat them into np.array """
-        assert self.players != None
-        self.state = np.concatenate(
-            [[np.array(player.population) for player in self.players]]
-        )
-        return True
-
-    def set_strategy_profile(self, strategy_profile=[0, 1, 1]):
-        """ Use a vector of pure strategies to assign it to players """
-        assert len(strategy_profile) == len(
-            self.players
-        ), f"Length of strategy_profile {len(strategy_profile)} does not match the amount of players {len(self.players)}"
-        for i, strategy in enumerate(strategy_profile):
-            self.players[i].strategy = strategy
-        self.strategy_profile = [player.strategy for player in self.players]
-        return True
-
-    def apply_payoff_matrix(self):
-        """ Applies payoffs to all players """
-        log.info(
-            f"{self.__class__}.apply_payoff_matrix() using payoff matrix {self.payoff_matrix.tolist()}"
-        )
-        for i, payoff in enumerate(self.payoff_matrix):
-            log.debug(
-                f"{self.__class__}.apply_payoff_matrix() player {i} old state {self.players[i].population}"
-            )
-            log.debug(
-                f"{self.__class__}.apply_payoff_matrix() player {i} should get {payoff}"
-            )
-            self.players[i].apply_player_payoff(payoff)
-            log.debug(
-                f"{self.__class__}.apply_payoff_matrix() player {i} new state {self.players[i].population}"
-            )
-
-    @classmethod
-    def get_payoff_matrix(self):
-        return self
 
 class PolymatrixGame(MultiplayerGame):
     def play(self, strategy_profile):
@@ -116,7 +37,7 @@ class PolymatrixGame(MultiplayerGame):
 
     def pair_fractional(self, player1: int, player2: int):
         """ Computer payoff between two players (one edge) """
-        alpha = 1/len(self.players)
+        alpha = 1 / len(self.players)
         p1 = self.players[player1]
         p2 = self.players[player2]
         p1_payoff = np.zeros(2)
@@ -150,90 +71,12 @@ class PolymatrixGame(MultiplayerGame):
     def payoff_function(self, x: int, alpha: float = 0.1, roundoff=True):
         """ A function that decides how much a player looses """
         # !!! alpha is overriden b self.alpha
-        y = x * self.alpha / (len(self.players)-1)
-        #print(f'Payoff function x={x},alpha={self.alpha},not rounded y={y}')
+        y = x * self.alpha / (len(self.players) - 1)
+        # print(f'Payoff function x={x},alpha={self.alpha},not rounded y={y}')
         if roundoff:
             y = int(y)
         assert y >= 0
         return y
-
-class DiscreteGame(MultiplayerGame):
-    def play(self, strategy_profile):
-        """ Wrapper method for setting a strategy profile, computing payoff and distributing it to players """
-        self.set_strategy_profile(strategy_profile)
-        self.get_payoff_matrix()
-        self.apply_payoff_matrix()
-        self.get_state()
-
-    def print(self):
-        for c in self.players:
-            print("Player ", c.index, c.company, " score ", sum(c.company))
-
-    def get_payoffs(self):
-        self.get_state()
-        base_state = self.state.copy()
-        number_of_players = len(self.players)
-
-        payoff = self.payoffs
-        strategies = all_binary_strategies(length=len(self.players))
-
-        for strategy in strategies:
-            str_strategy = "".join(map(str, strategy))
-            self.players = []
-            player_state = []
-            # [list(base_state[:,0]),list(base_state[:,1]),list(base_state[:,2])]
-            for i in range(number_of_players):
-                player_state.append(list(base_state[:, i]))
-            # print(player_state)
-            self.player_generator(player_state)
-            self.update_strategies(strategy)
-            self.round()
-            self.get_state()
-            difference = self.state - base_state
-            difference = np.sum(difference, axis=0)
-            payoff[str_strategy] = list(difference)
-            self.payoffs = payoff
-
-    def get_nash(self):
-        nash = self.nash
-        strategies = all_binary_strategies(length=len(self.players))
-
-        for strategy in strategies:
-            str_strategy = "".join(map(str, strategy))
-            nash[str_strategy] = self.is_strategy_nash(strategy)
-
-    def is_strategy_nash(self, strategy):
-        """
-        For strategy being a list of binary values corresponding to 
-        """
-        is_nash = True
-        base_payoff = self.payoff[
-            "".join(map(str, strategy))
-        ]  # np.sum(self.state, axis=0)
-        for i in range(len(strategy)):
-            compared_strategy = strategy.copy()
-            compared_strategy[i] = 1 - compared_strategy[i]  # flips 1 and 0
-            compare_payoff = self.payoff["".join(map(str, compared_strategy))]
-            if base_payoff[i] < compare_payoff[i]:
-                is_nash = False
-            # print(strategy, compared_strategy, base_payoff[i]<compare_payoff[i])
-        return is_nash
-
-    def __call__(self):
-        self.get_payoffs()
-        return True
-        # print('Payoffs:', self.payoff)
-
-    def show_nash(self):
-        is_nash = True
-        if self.nash == {}:
-            print("Nash empty!")
-            is_nash = False
-        self.get_nash()
-        for key in self.nash:
-            if self.nash[key] == True:
-                print("Nash:", key, " with payoff ", self.payoff[key])
-        return is_nash
 
 
 def all_binary_strategies(length=3):
@@ -277,15 +120,19 @@ def simulate(
     return population
 
 
-class GameManager():
-    def __init__(self, number_of_players = 3):
+class GameManager:
+    def __init__(self, number_of_players=3):
         self.number_of_players = number_of_players
         pass
+
     def get_random_players(self):
         n = self.number_of_players
-        players = [[random.randint(n, n*10),random.randint(n, n*10)] for i in range(n)]
+        players = [
+            [random.randint(n, n * 10), random.randint(n, n * 10)] for i in range(n)
+        ]
         return players
+
     def get_random_strategy_profile(self):
         n = self.number_of_players
-        strategy_profile = [random.randint(0,1) for i in range(n)]
+        strategy_profile = [random.randint(0, 1) for i in range(n)]
         return strategy_profile
---
+++ multiplayergame.py
@@ -0,0 +1,93 @@
+import os, random
+import numpy as np
+from logger import log
+from player import Player
+from network import Network
+
+
+class MultiplayerGame:
+    """ A multiplayer game with state vector for each player """
+
+    def __init__(
+        self,
+        start_populations_matrix=[],
+        topology="fully_connected",
+        alpha=0.5,
+        log_level="info",
+    ):
+        self.alpha = alpha
+        self.players = None
+        self.network = None
+        self.loss_velocity = None
+        self.strategy_profile = None
+        self.payoff_matrix = None
+        self.payoffs = {}
+        self.state = None
+        self.nash = {}
+        log.setLevel(log_level)
+        if start_populations_matrix != []:
+            self.initiate_players(start_populations_matrix=start_populations_matrix)
+            self.get_state()
+            self.players_indices = [player.index for player in self.players]
+            log.info(
+                f"{self.__class__}.__init__() players indexed {self.players_indices}"
+            )
+            self.network = Network(nodes=self.players_indices, topology=topology)
+        else:
+            log.warning(
+                f"{self.__class__}.__init__() players and network not initiated"
+            )
+
+    def initiate_players(self, start_populations_matrix: list, player_names_list=None):
+        """ Adds players to the game one by one """
+        assert (
+            self.players == None
+        )  # make sure that there aren't any players in the game
+        assert type(start_populations_matrix) == list
+        assert all(
+            len(x) == len(start_populations_matrix[0]) for x in start_populations_matrix
+        )
+        players_list = []
+        for i, population_vector in enumerate(start_populations_matrix):
+            players_list.append(Player(population_vector=population_vector, index=i))
+        self.players = players_list
+        return True
+
+    def get_state(self):
+        """ Extract state of each player and concat them into np.array """
+        assert self.players != None
+        self.state = np.concatenate(
+            [[np.array(player.population) for player in self.players]]
+        )
+        return True
+
+    def set_strategy_profile(self, strategy_profile=[0, 1, 1]):
+        """ Use a vector of pure strategies to assign it to players """
+        assert len(strategy_profile) == len(
+            self.players
+        ), f"Length of strategy_profile {len(strategy_profile)} does not match the amount of players {len(self.players)}"
+        for i, strategy in enumerate(strategy_profile):
+            self.players[i].strategy = strategy
+        self.strategy_profile = [player.strategy for player in self.players]
+        return True
+
+    def apply_payoff_matrix(self):
+        """ Applies payoffs to all players """
+        log.info(
+            f"{self.__class__}.apply_payoff_matrix() using payoff matrix {self.payoff_matrix.tolist()}"
+        )
+        for i, payoff in enumerate(self.payoff_matrix):
+            log.debug(
+                f"{self.__class__}.apply_payoff_matrix() player {i} old state {self.players[i].population}"
+            )
+            log.debug(
+                f"{self.__class__}.apply_payoff_matrix() player {i} should get {payoff}"
+            )
+            self.players[i].apply_player_payoff(payoff)
+            log.debug(
+                f"{self.__class__}.apply_payoff_matrix() player {i} new state {self.players[i].population}"
+            )
+
+    @classmethod
+    def get_payoff_matrix(self):
+        return self
